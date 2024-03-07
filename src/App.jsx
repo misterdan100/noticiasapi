61791f4d12af08ff2466f35e50767c4e4390cbec
misterdan100 <danielmca60@gmail.com>
2024-03-07 06:44:45 -0500

    Setting MUI React UI, creating input and label

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-
+import { Container, Grid, Typography } from "@mui/material";
+import Formulario from "./components/Formulario";
 
 function App() {
-
-
   return (
-    <>
-      <h1>App Noticias</h1>
-    </>
-  )
+    <Container>
+      <header>
+        <Typography align="center" marginY={5} component="h1" variant="h3">
+          Buscador de Noticias
+        </Typography>
+      </header>
+
+      <Grid
+        container
+        direction='row'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Grid item xs={12} md={6} lg={4}>
+          <Formulario />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export default App
+export default App;
